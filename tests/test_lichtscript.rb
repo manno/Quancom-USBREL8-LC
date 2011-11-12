d83b5287514a7a3818e057c070e941f15ce50a09
Mario Manno <mm@koeln.ccc.de>
2011-11-12 17:23:40 +0100

    create second test script

---
+++ tests/test_lichtscript.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+$LOAD_PATH << './lib'
+require 'lichtscript'
+require 'pp'
+
+script = <<-EOF
+label output 3 as Bathroom
+turn off Bathroom
+turn off all outputs
+turn off all outputs in 1 minute
+turn on output1 
+turn on output 1 
+turn on output 1, Bathroom, output 2
+turn off output 1, Bathroom, output 2
+turn on Bathroom
+turn on Bathroom in 5 minutes for 45 seconds
+turn on Bathroom in 1 hour 
+EOF
+
+licht = Licht::Script.load( script )
+pp licht
